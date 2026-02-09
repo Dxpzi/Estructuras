c033ea4569b654f113eda3482fc7920f4b0bcf93
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:11:17 -0500

    Proyecto estructura de datos

--- nacholee.java
+++ nacholee.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class nacholee {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Vamos a sumar dos números :)");
+
+        int n1 = 0, n2 = 0, Suma = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el número 1");
+        n1 = sc.nextInt();
+
+        System.out.println("Ingrese el número 2");
+        n2 = sc.nextInt()
+
+        System.out.println("La suma de los números es: " + (n1 + n2));
+        
     }
 }
